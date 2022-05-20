0400aa41e410c45bcb45da80578fa59311bf9c76
Soojin Kim <soojinkk98@gmail.com>
2022-05-20 14:00:44 -0400

    Allow users to draw with Etch-a-Sketch with aqua color

---
+++ external.js
@@ -0,0 +1,43 @@
+const container = document.querySelector('#container');
+const div = document.querySelector('div');
+const button = document.querySelector('button');
+const color = document.querySelector('.color');
+
+function setupDivs(x) {
+    for (let i=0; i < x; i++) {
+        const outerdiv = document.createElement('div');
+        outerdiv.classList.add('outerdiv');
+        container.appendChild(outerdiv);
+        for (let i=0; i<x; i++) {
+            const innerdiv = document.createElement('div');
+            innerdiv.classList.add('innerdiv');
+            outerdiv.appendChild(innerdiv);
+        }
+    }
+};
+
+setupDivs(16);
+
+div.addEventListener('mouseover', function(e) {
+    e.target.classList.add('color');
+});
+
+button.addEventListener('click', function(e) {
+    
+    document.getElementById('container').replaceChildren();
+    let x = document.getElementById("numSquares").value;
+    if (x > 100 || x < 0) {
+        alert("Please input a value between 1 and 100");
+        setupDivs(16);
+        return;
+    }
+    else if (isNaN(x)) {
+        alert("Please input a numeric value");
+        setupDivs(16);
+        return;
+    }
+
+    setupDivs(x);
+});
+
+
